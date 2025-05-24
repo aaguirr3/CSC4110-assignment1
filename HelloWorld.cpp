15ea731deb5698ddd08005ad6af8134cead13de8
Andrea Aguirre <aguirreandrea988@gmail.com>
2025-05-23 23:03:55 -0400

    Changing the greetings

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,5 +1,5 @@
 // Simple C++ program to display "Hello World"
-
+// Author: Andrea Aguirre <hh9241@wayne.ed>
 //Header file for input output functions
 #include <iostream>
 
@@ -10,7 +10,7 @@ using namespace std;
 int main() 
 {
   //prints hello world
-    cout << "Hello World";
+    cout << "testing git merge!";
 
     return 0;
 }
